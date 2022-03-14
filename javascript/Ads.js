cd3f87b7e82df6db2d281ed0c76ee89ad3a434bb
Ahmad Wahyudi <84139744+wahyu9kdl@users.noreply.github.com>
2022-03-14 20:33:13 +0700

    Update Ads.js

--- javascript/Ads.js
+++ javascript/Ads.js
@@ -1,4 +1,4 @@
-var jumlah_iklan = 10;
+var jumlah_iklan = 20;
 var waktu_sekarang = new Date()
 var detik = waktu_sekarang.getSeconds(3)
 var pasang_iklan = detik % jumlah_iklan;
@@ -80,6 +80,80 @@ if (pasang_iklan == 10) {
     alt = "Ads";
     title = "Ads";
     banner = "https://imp.accesstra.de/img.php?rk=00167y000n9y";
+    width = "300";
+    height = "250";
+}
+if (pasang_iklan == 11) {
+    url = "https://www.alhikmah.my.id/2020/10/pentingnya-logo-untuk-sebuah-websiteap.html";
+    alt = "ALHIKMAH.MY.ID";
+    title = "Pentingnya Logo Untuk Sebuah Website";
+    banner = "https://1.bp.blogspot.com/-uQ5Q7qYybMs/X5EYN62EbdI/AAAAAAAABdU/ObOnjBrQ4-MwITVPZ30CXnzgKx2j9LKggCLcBGAsYHQ/s958/Kata%2Bkata%2Bhikmah_compress84_crop_26_compress74.jpg";
+    width = "300";
+    height = "250";
+}
+
+if (pasang_iklan == 12) {
+    url = "https://www.alhikmah.my.id/2022/02/rajab.html";
+    alt = "ALHIKMAH.MY.ID";
+    title = "12 Amalan Bulan Rajab yang Dianjurkan Untuk dikerjakan Oleh Umat Islam";
+    banner = "https://blogger.googleusercontent.com/img/a/AVvXsEjQi8Bs641f7yUqZAOfZsSdi31o29iA7XTBCX7g1vl3GlxHze2iD9nXHq85Fvyn3VTAKaAW3NqdyXm35KNHWLZUvWCF8zryX0TSefu1Ks_Ic0RRh3_USIlPsyhJ2byGDovfQaKrgzLOf1Z88p0An75WGU3Jer81EWq87bSZO99J9X4CoCHy5fZhz6oEXw=s810";
+    width = "300";
+    height = "250";
+}
+if (pasang_iklan == 13) {
+    url = "https://www.alhikmah.my.id/2020/08/irmas-ikatatan-remaja-masjid.html";
+    alt = "ALHIKMAH.MY.ID";
+    title = "IRMAS (IKATATAN REMAJA MASJID )";
+    banner = "https://1.bp.blogspot.com/-_IDSby0Rdm8/XygQt-pVK1I/AAAAAAAAAM0/6enO4QHvPeoRgxdn79y95Snz7z0NsjZVgCLcBGAsYHQ/w400-h287/foto-masjid-agung-tempo-dulu_ixa3xv.jpg";
+    width = "300";
+    height = "250";
+}if (pasang_iklan == 14) {
+    url = "https://www.alhikmah.my.id/2020/10/meraih-penghasilan-dengan-google-adsense.html";
+    alt = "ALHIKMAH.MY.ID";
+    title = "Cara Meraih Penghasilan dengan Google AdSense - News";
+    banner = "https://blogger.googleusercontent.com/img/a/AVvXsEj2Viyk7dFY5sywpnRn3zMEGkEn7ZU9mXcxHTFqEENjUFPD0wzxBNAEiyTgP7Cq_pNhbLwEpX0pEZz3llOmOF09lbhs6y0MGUizDqtUCDDzezXuZFvLbsOOsvKa6uUm1p-fGA-yLOmynFV2T4jxM37HAzgEaDUG0-Q2IhEGne7aBkMAZGgWLkRnM7CjXQ=s720";
+    width = "300";
+    height = "250";
+}if (pasang_iklan == 15) {
+    url = "https://www.alhikmah.my.id/2022/02/mazer.html";
+    alt = "ALHIKMAH.MY.ID";
+    title = "Download MP3 & Video For: Download Video Klip Album Mazer Zain Mp4 Gratis Terbaru";
+    banner = "https://blogger.googleusercontent.com/img/a/AVvXsEhDaLSLOSNSlyjrDcazdjMAh2aK0SXzQxe4zYXnSfZp5nIbW6faEEG0dmz5ezaaVU12kLUzW41RcTbSXJFtoYa3Rc9Yv_lf7lTogL8FkxHY2BID2979ZAxCRTSPX0CtyX7D5HyEQccUOWgUgR53n4oRyMosO6UtH-pDrnn_tA488gB5z-qtBqL1QsCIRA=s554";
+    width = "300";
+    height = "250";
+}if (pasang_iklan == 16) {
+    url = "https://www.alhikmah.my.id/2022/02/sholawat32.html";
+    alt = "ALHIKMAH.MY.ID";
+    title = "Al-Jannatu Wa Na’imuha Sa’dun - Sholawat(32)";
+    banner = "https://blogger.googleusercontent.com/img/a/AVvXsEh-AFZFYt-j8Bk_qeIWOFY-Zdq5xFPR23U80hid3ivRlbAgpZ-YvvLXLrpQTlVsbh2AKEsGpErVvPxj826Cv1GMRZ0f2ZsMB0VBupVrSjSQl76q_iuhBdiryXi7IHhaO32BqRIq-llApAyPl9W5ghhFtEg1pVDAFHchGS7wkUqTFDBgGMLgMpk7WTrJiw=s236";
+    width = "300";
+    height = "250";
+}if (pasang_iklan == 17) {
+    url = "https://www.alhikmah.my.id/2021/12/sholawat1.html";
+    alt = "ALHIKMAH.MY.ID";
+    title = "Aini Lighoiri Jamalikum - Sholawat(1)";
+    banner = "https://blogger.googleusercontent.com/img/a/AVvXsEiP7prnjUl7uXxkZihD2y6YqTaj3u0pz9HHEcu0vFeSGbWy7OieCTzW5nIXpJ2vh6CwbdFchWHu6ggp0jXDwcpEyhx4N6rc77MCWTVlD6pBARwwocK_nJ6EFAwKqJml8IiuQ8Ttcnrj5fNCVQ102J8TPTzxuIY7PFUCxJiXxVibEYHa6r1B2Og9tWI9hQ=s640";
+    width = "300";
+    height = "250";
+}if (pasang_iklan == 18) {
+    url = "https://www.alhikmah.my.id/2022/02/sholawat35.html";
+    alt = "ALHIKMAH.MY.ID";
+    title = "Allah Allah Wassholatu ‘Alal Mukhtar - Sholawat(35)";
+    banner = "https://blogger.googleusercontent.com/img/a/AVvXsEh-AFZFYt-j8Bk_qeIWOFY-Zdq5xFPR23U80hid3ivRlbAgpZ-YvvLXLrpQTlVsbh2AKEsGpErVvPxj826Cv1GMRZ0f2ZsMB0VBupVrSjSQl76q_iuhBdiryXi7IHhaO32BqRIq-llApAyPl9W5ghhFtEg1pVDAFHchGS7wkUqTFDBgGMLgMpk7WTrJiw";
+    width = "300";
+    height = "250";
+}if (pasang_iklan == 19) {
+    url = "https://www.alhikmah.my.id/2022/01/images.html";
+    alt = "ALHIKMAH.MY.ID";
+    title = "KALIGRAFI LAFADZ MUHAMMAD";
+    banner = "https://www.alhikmah.my.id/2022/01/images.html";
+    width = "300";
+    height = "250";
+}if (pasang_iklan == 20) {
+    url = "https://www.alhikmah.my.id/2021/09/kitab-kuning.html";
+    alt = "ALHIKMAH.MY.ID";
+    title = "KITAB KUNING LENGKAP - AL HIKMAH | GRATIS DOWNLOAD";
+    banner = "https://1.bp.blogspot.com/-N-_5JTd9pMM/YTwit_FXahI/AAAAAAAAECY/RhhVOQLVssEtlRf6EUjYGaClLOzgs15RgCLcBGAsYHQ/s800/Kitab%2BKuning%257EAw.png";
     width = "300";
     height = "250";
 }
